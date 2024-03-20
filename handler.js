93c0cbe73028226277c9e30d5f5443019d9e6edb
Antonio Semeraro <antsemeraro@gmail.com>
2024-03-20 15:49:31 +0100

    Update serverless file

--- handler.js
+++ handler.js
@@ -1,37 +1,47 @@
-"use strict";
-const { Client } = require("pg");
+'use strict';
+const { Client } = require('pg');
 
-const allTokenUpdate = require("./src/allTokenUpdate");
+const allTokenUpdate = require('./src/allTokenUpdate');
 const allTokenUpdateHandler = async (event) => {
   await allTokenUpdate();
 };
 
-const kdaPriceUpdate = require("./src/kdaPriceUpdate");
+const allKadenaTokenUpdate = require('./src/allKadenaTokenUpdate');
+const allKadenaTokenUpdateHandler = async (event) => {
+  await allKadenaTokenUpdate();
+};
+
+const kdaPriceUpdate = require('./src/kdaPriceUpdate');
 const kdaPriceUpdateHandler = async (event) => {
   const client = new Client();
   await client.connect();
   await kdaPriceUpdate(client);
 };
 
-const candleUpdate = require("./src/candleUpdate");
+const candleUpdate = require('./src/candleUpdate');
 const candleUpdateHandler = async (event) => {
   const client = new Client();
   await client.connect();
   await candleUpdate(client);
 };
 
-const hourCandlesUpdate = require("./src/hourCandlesUpdate");
+const hourCandlesUpdate = require('./src/hourCandlesUpdate');
 const hourCandlesUpdateHandler = async (event) => {
   const client = new Client();
   await client.connect();
   await hourCandlesUpdate(client);
 };
 
-const highLowUpdate = require("./src/highLowUpdate");
+const highLowUpdate = require('./src/highLowUpdate');
 const highLowUpdateHandler = async (event) => {
   const client = new Client();
   await client.connect();
   await highLowUpdate(client);
+};
+
+const updateAccountsBalance = require('./src/accountsBalanceUpdate');
+const updateAccountsBalanceHandler = async (event) => {
+  await updateAccountsBalance();
 };
 
 module.exports = {
@@ -40,4 +50,6 @@ module.exports = {
   candleUpdateHandler,
   hourCandlesUpdateHandler,
   highLowUpdateHandler,
+  allKadenaTokenUpdateHandler,
+  updateAccountsBalanceHandler,
 };
